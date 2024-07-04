af6efa0b97afc69313d7f1654a45ada163ceb180
sekorme <asiedua4@uni.coventry.ac.uk>
2024-07-04 00:32:00 +0000

    next one

--- components/OverView.tsx
+++ components/OverView.tsx
@@ -32,7 +32,7 @@ export function OverView() {
           <CardHeader>
             <CardTitle className="text-green-600">Presidential Election Results</CardTitle>
             <CardDescription >
-              overview of the presidential  election results
+              overview of the presidential now  election results
             </CardDescription>
           </CardHeader>
           <CardContent className="grid grid-cols-1 gap-1 md:grid-cols-2 sm:grid-cols-1 w-full items-center justify-center ">
