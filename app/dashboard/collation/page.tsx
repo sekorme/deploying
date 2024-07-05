188d4ee32416f955110aba3db5e88ccdaa815e49
sekorme <asiedua4@uni.coventry.ac.uk>
2024-07-05 21:40:25 +0000

    alwasy2

--- app/dashboard/collation/page.tsx
+++ app/dashboard/collation/page.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 
 const Collation = () => {
   return (
-    <div className="mt-20 md:mt-2">Collation</div>
+    <>
+    <Card>
+      <CardHeader>
+        <CardTitle>Collation</CardTitle>
+        <CardDescription>Collation of the election results.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div>
+          Collation
+        </div>
+      </CardContent>
+    </Card>
+    </>
   )
 }
 
